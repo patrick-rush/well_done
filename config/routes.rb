9769e2c110d87287523070a955d8a2df979b8566
patrick-rush <patrick.a.rush@gmail.com>
2020-11-02 17:33:03 -0500

    add devise init config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# When models/controllers have been created, complete this task so that devise gem works correctly
+# 2. Ensure you have defined root_url to *something* in your config/routes.rb.
+# For example:
+
+#   root to: "home#index"
+
+# * Not required for API-only Applications *
