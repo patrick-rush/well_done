927c3f698fd37c5ce1f1cab266e7d577f15c7737
patrick-rush <patrick.a.rush@gmail.com>
2020-11-05 20:58:21 -0500

    update user validations and first/last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
   has_many :tasks
   has_many :projects, through: :tasks
 
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, presence: true, uniqueness: true
 
   def full_name
     "#{first_name} #{last_name}"
@@ -16,13 +17,27 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      # user.name = auth.info.name
+      user.first_name = auth.info.name.split(/ /).first
+      user.last_name = auth.info.name.split(/ /).last
       user.password = Devise.friendly_token[0, 20]
       # user.name = auth.info.name   # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
-      user.skip_confirmation!
+      # user.skip_confirmation!
     end
+
+    # def apply_omniauth(auth)
+    #   update_attributes(
+    #     provider: auth.provider,
+    #     uid: auth.uid
+    #   )
+    # end
+
+    # def has_facebook_linked?
+    #   self.provider.present? && self.uid.present?
+    # end
 
     # devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
