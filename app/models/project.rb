1c2b0fb239ee9fed44e9a76a4280894977828420
patrick-rush <patrick.a.rush@gmail.com>
2020-11-16 09:16:17 -0600

    styles and cascade delete dependant tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-    has_many :tasks
+    has_many :tasks, dependent: :delete_all
     accepts_nested_attributes_for :tasks
     has_many :users, through: :tasks
     has_many :comments, as: :commentable
